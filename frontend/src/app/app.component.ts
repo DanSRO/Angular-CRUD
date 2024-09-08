d03e9119114ed4697766e24bcefff71912248d96
daniel dantas <Daniel Dantas>
2024-09-08 01:25:15 -0300

    lib material, assets, style, components header and footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import "../styles.css";
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MatToolbarModule],
   template: `
     <h1>Bem-vindo ao projeto {{title}}!</h1>
 
